eb5958fc301e453289b739877d33f50c2b96a4f8
rdw75 <77143203+rdw75@users.noreply.github.com>
2025-03-25 10:35:04 +0700

    Update entrypoint_with_four_interfaces.sh
    Try to run 128Mb of RAM

--- scripts/entrypoint_with_four_interfaces.sh
+++ scripts/entrypoint_with_four_interfaces.sh
@@ -2,7 +2,7 @@
 
 qemu-system-x86_64 \
     -vnc 0.0.0.0:0 \
-    -m 256 \
+    -m 128 \
     -hda $ROUTEROS_IMAGE \
     -device e1000,netdev=net0 \
     -netdev user,id=net0,hostfwd=tcp::21-:21,hostfwd=tcp::22-:22,hostfwd=tcp::23-:23,hostfwd=tcp::50-:50,hostfwd=udp::50-:50,hostfwd=tcp::51-:51,hostfwd=udp::51-:51,hostfwd=tcp::80-:80,hostfwd=tcp::443-:443,hostfwd=udp::500-:500,hostfwd=tcp::1194-:1194,hostfwd=udp::1194-:1194,hostfwd=tcp::1701-:1701,hostfwd=udp::1701-:1701,hostfwd=tcp::1723-:1723,hostfwd=udp::1723-:1723,hostfwd=udp::1812-:1812,hostfwd=udp::1813-:1813,hostfwd=udp::4500-:4500,hostfwd=tcp::8080-:8080,hostfwd=tcp::8291-:8291,hostfwd=tcp::8728-:8728,hostfwd=tcp::8729-:8729,hostfwd=tcp::8900-:8900 \
